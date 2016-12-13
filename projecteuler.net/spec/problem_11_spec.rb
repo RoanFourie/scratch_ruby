24e5f52477bff88975090b893771cdc1026e8c37
Roan Fourie <roanfourie@gmail.com>
2016-12-13 12:33:48 +0200

    projecteuler.net: Removed spec for puzzle 11, did not use a spec

--- projecteuler.net/spec/problem_11_spec.rb
+++
@@ -1,7 +0,0 @@
-require_relative "../problem_11"
-  describe "Find the largest product in a grid"
-    describe "largest_product_grid" do
-      it "Returns" do
-        expect(largest_product_grid(2)).to eq(9009)
-      end
-    end
