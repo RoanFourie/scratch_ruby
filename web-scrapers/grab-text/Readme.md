c64957e26758f74cc0ce9c6ea02a7960925d9216
Roan Fourie <roanfourie@gmail.com>
2017-02-02 16:10:33 +0200

    Added the grab text web scraper and updated Youtube downloader

---
+++ web-scrapers/grab-text/Readme.md
@@ -0,0 +1,4 @@
+# Web Scraper that grabs text
+## Better Web Scraping with Nokogiri
+
+[Original at jasonheppler.org](http://jasonheppler.org/2012/10/12/better-web-scraping-with-nokogiri/)
--- youtube-get-vids/get-vids.rb
+++ youtube-get-vids/get-vids.rb
@@ -18,23 +18,34 @@ f.each_line {|line| list_of_videos << line}
 # go through each item in list and download it
 list_of_videos.each do |video|
   unless video[0] == "#"
-    100.times {putc("*")}
-    puts("Started with #{video}")
+    50.times {putc("*")}
+    puts("\nStarted with #{video}")
     # execute the command in windows, caling youtube-dl
-    system(app_path, video)
+    system(app_path, video, "-R 3", "--write-description", "--write-info-json")
+    # -R = retries
   else
-    100.times {putc("*")}
-    puts("video")
+    50.times {putc("*")}
+    puts("\n#{video}")
   end
+  sleep(3)
 end
-
+50.times { putc("*")}
+puts "\ncompleted"
+nothing = gets.chomp
 
 # INFORMATION
 # https://github.com/rg3/youtube-dl/blob/master/README.md#options
 
-# You can configure youtube-dl by placing any supported command line option to a configuration file. On Linux and OS X, the system wide configuration file is located at /etc/youtube-dl.conf and the user wide configuration file at ~/.config/youtube-dl/config. On Windows, the user wide configuration file locations are %APPDATA%\youtube-dl\config.txt or C:\Users\<user name>\youtube-dl.conf. Note that by default configuration file may not exist so you may need to create it yourself.
+# You can configure youtube-dl by placing any supported command line option to
+# a configuration file. On Linux and OS X, the system wide configuration file is
+# located at /etc/youtube-dl.conf and the user wide configuration file at
+# ~/.config/youtube-dl/config. On Windows, the user wide configuration file locations
+# are %APPDATA%\youtube-dl\config.txt or C:\Users\<user name>\youtube-dl.conf.
+# Note that by default configuration file may not exist so you may need to create it yourself.
 #
-# For example, with the following configuration file youtube-dl will always extract the audio, not copy the mtime, use a proxy and save all videos under Movies directory in your home directory:
+# For example, with the following configuration file youtube-dl will always
+# extract the audio, not copy the mtime, use a proxy and save all videos under
+# Movies directory in your home directory:
 #
 # # Lines starting with # are comments
 #
@@ -52,6 +63,9 @@ end
 #
 # # Save all videos under Movies directory in your home directory
 # -o ~/Movies/%(title)s.%(ext)s
-# Note that options in configuration file are just the same options aka switches used in regular command line calls thus there must be no whitespace after - or --, e.g. -o or --proxy but not - o or -- proxy.
+# Note that options in configuration file are just the same options aka switches
+# used in regular command line calls thus there must be no whitespace after - or
+# --, e.g. -o or --proxy but not - o or -- proxy.
 #
-# You can use --ignore-config if you want to disable the configuration file for a particular youtube-dl run.
+# You can use --ignore-config if you want to disable the configuration file for
+# a particular youtube-dl run.
